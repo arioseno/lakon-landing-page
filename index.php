644f0acff1058ac432660f7ccb7a8686e90e4009
arioseno <31603990+arioseno@users.noreply.github.com>
2017-09-06 16:11:08 +0700

    Landing Page

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link rel="stylesheet" href="css/main-lakon.css">
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link href="https://fonts.googleapis.com/css?family=Lora|Oxygen+Mono|Pacifico|Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/lity.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/animate.min.css">
+</head>
+
+<body>
+	<nav>
+		<div class="nav-wrapper">
+			<div class="container wow fadeIn" data-wow-delay="1.5s">
+				<a href="#" class="brand-logo center"><span style="font-family: 'Pacifico', cursive;">Lumen</span><span style="font-family: 'Oxygen Mono', monospace;">AI</span>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a href="#getintouch">Get In Touch</a></li>
+					</ul>
+				</div>
+			</div>
+	</nav>
+		<!-- hero start -->
+		<div class="hero-wrapper">
+			<div class="container container-pad">
+				<div class="row">
+					<div class="ln-hero-wrapper center-align">
+						<div class="col s12 m12 ">
+							<div class="ln-hero-title ">
+								<h1>Great Things Start Here.</h1>
+							</div>
+							<div class="ln-sub-hero-title">
+								We Build Awesome Products based on <i>Artificial Intelligence.</i>
+							</div>
+							<div class="ln-btn-wrapper">
+								<a class="waves-effect waves-light btn ln-hero-btn">What Can We Do?</a>
+							</div>
+						</div>
+<!-- 	<div class="col s12 m6 center-align">
+<img src="img/hero-working2.svg" alt="hero-working" height="277.5" class="">
+</div> -->
+</div>
+</div>
+</div>
+</div>
+<!-- end of hero -->
+<!-- start of section 2 -->
+<div class="ln-sec-2">
+	<div class="container container-pad">
+		<h1 class="ln-heading center">What Can We Do?</h1>
+		<div class="row">
+			<div class="col s12 m4 thumb-down wow fadeInUp" data-wow-delay="0.5s">
+				<div class="icon-block">
+					<div class="center">
+						<img src="img/svg/006-atomic.svg" alt="" height="106.67" class="center">
+					</div>
+					<div class="ln-sub-heading center"><h2>Machine Learning</h2></div>
+					<div class="ln-sub-title center">
+						<h5>Lumen Berbasis Kepintaran Buatan.</h5>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m4 thumb-down wow fadeInUp" data-wow-delay="1s"">
+				<div class="icon-block">
+					<div class="center">
+						<img src="img/svg/005-startup.svg" alt="" height="106.67" class="center">
+					</div>
+					<div class="ln-sub-heading center"><h2>Well Integrated</h2></div>
+					<div class="ln-sub-title center">
+						<h5>
+							Telah terintergasi dengan baik dengan beberapa program pemerintah.</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col s12 m4 thumb-down wow fadeInUp" data-wow-delay="1.5s"">
+					<div class="icon-block">
+						<div class="center">
+							<img src="img/svg/004-chat.svg" alt="" height="106.67" class="center">
+						</div>
+						<div class="ln-sub-heading center"><h2>ChatBot</h2></div>
+						<div class="ln-sub-title center">
+							<h5>
+								Dapat Melayani Lewat Pesan Instan maupun media sosial.</h5>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- endofsection2 -->
+		<!-- startofsection3 -->
+		<div class="">
+			<div class="ln-sec-3">
+				<div class="container container-pad">
+					<div class="row">
+						<div class="col m4 s12">
+							<div class="ln-about-title wow fadeInDown" data-wow-delay="0.5s">
+								<h1>Why Lumen?</h1>
+							</div>
+							<div class="ln-about-content wow fadeIn" data-wow-delay="1s">
+								<h3>Dengan Lumen Helpdesk. Pekerjaan yang biasa dilakukan manusia akan digantikan robot yang berbasis kepintaran buatan atau Artificial Intelegence dengan cara melayani melewati pesan instan (chat).</h3>
+							</div>
+							<div class="ln-btn-wrapper">
+								<a class="waves-effect waves-light btn ln-about-btn" href="https://www.youtube.com/watch?v=naZ-othOfuU" data-lity>Lihat Video</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- endofsection3 -->
+		<!-- startofsection4 -->
+		<div class="ln-sec-4">
+			<div class="container container-pad">
+				<h1 class="ln-heading center">Our Milestones</h1>
+				<div class="row">
+					<div class="col s12 m4 thumb-down">
+						<div class="icon-block">
+							<div class="center">
+								<img src="img/svg/003-trophy.svg" alt="" height="90" class="center">
+							</div>
+							<div class="ln-ml-heading center"><h2><span class="stat-count">14</span></h2></div>
+							<div class="ln-ml-title center">
+								<h5>Awards winning.</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m4 thumb-down">
+						<div class="icon-block">
+							<div class="center">
+								<img src="img/svg/002-podium.svg" alt="" height="90" class="center">
+							</div>
+							<div class="ln-ml-heading center"><h2><span class="stat-count">20</span></h2></div>
+							<div class="ln-ml-title center">
+								<h5>
+									happy customers</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col s12 m4 thumb-down">
+							<div class="icon-block">
+								<div class="center">
+									<img src="img/svg/001-coffee-cup.svg" alt="" height="90" class="center">
+								</div>
+								<div class="ln-ml-heading center"><h2><span class="stat-count">5000</span></h2></div>
+								<div class="ln-ml-title center">
+									<h5>
+										Ordered Coffee's</h5>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- endofsection4 -->
+				<!-- startofsection5 -->
+				<div class="ln-sec-5">
+					<div class="container">
+						<div class="ln-sec-5-rised">
+							<h1 class="ln-heading center">Get in Touch</h1>
+							<div class="row">
+								<div class="col s12 m12 center">
+									<div class="ln-contact-box">
+										<b>Contact Us!</b>
+										<div class="row">
+											<div class="col s6"><p><i class="fa fa-phone mini-icon" aria-hidden="true"></i>+6281221673472 </p></div>
+											<div class="col s6"><p><i class="fa fa-envelope mini-icon" aria-hidden="true"></i>info@lumen-ai.com</p> </div>
+										</div>
+										<div class="row">
+											<div class="col s6"><p><i class="fa fa-github mini-icon" aria-hidden="true"></i><a href="https://twitter.com/lumenrobot">Twitter</a></p></div>
+											<div class="col s6"><p><i class="fa fa-twitter mini-icon" aria-hidden="true"></i><a href="https://github.com/lumenrobot">GitHub</a></p> </div>
+										</div>
+										<div class="row"><p><i class="fa fa-map-marker mini-icon" aria-hidden="true"></i>Bandung, Indonesia</p> </div>
+									</div>
+								</div>
+						<!-- 		<div class="col s12 m6">
+									<div class="ln-contact-box">
+										<form action="">
+											<div class="row"> 
+												<div class="input-field col s6">
+													<input id="name" type="text" class="validate" required="" name="name">
+													<label for="name">Your Name</label>
+													
+												</div>
+												<div class="input-field col s6">
+													<input id="email" type="email" class="validate" required="" name="email">
+													<label for="email">Last Name</label>
+												</div>
+											</div>
+											<div class="row">
+												<div class="input-field col s12">
+													<textarea id="textarea1" class="materialize-textarea" required="" name="message"></textarea>
+													<label for="message">Textarea</label>
+												</div>
+											</div>
+											<div class="row right-align">
+												<div class="input-field col s6">
+													<input id="human" type="text" class="validate" required="" name="human">
+													<label for="human">5+3 =</label>
+												</div>
+												<div class="input-field col s6">
+													<input id="submit" name="submit" type="submit" value="Submit" class="waves-effect waves-light btn">
+												</div>
+											</div>
+										</form>
+									</div>
+							      </div> -->
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				
+			</div>
+<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/jquery.typeit/4.4.0/typeit.min.js"></script>
+	<script src="js/lity.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script type="text/javascript" src="js/main-lakon.js"></script>
+	</body>
+</html>
